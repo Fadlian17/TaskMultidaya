bf093e64bd360d3790b139cffb012ce7434243e4
Fadlian17 <alfansyahfadlian@gmail.com>
2020-08-05 17:00:47 +0700

    fixed task android basic

--- app/src/main/java/com/alfansyah/multidaya/taskmultidaya/SecondActivity.java
+++ app/src/main/java/com/alfansyah/multidaya/taskmultidaya/SecondActivity.java
@@ -1,12 +1,15 @@
 package com.alfansyah.multidaya.taskmultidaya;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -31,6 +34,24 @@ public class SecondActivity extends AppCompatActivity {
         TextView tvdetail2 = (TextView)findViewById(R.id.TV2);
         if(getIntent().getExtras()!=null){
             tvdetail2.setText(getIntent().getStringExtra("data1"));
+            AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+            dialog.setMessage("Yes ,Data : "+ getIntent().getStringExtra("data1")+"is passed");
+            dialog.setTitle("Dialog Box");
+            dialog.setPositiveButton("YES",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,
+                                            int which) {
+                            Toast.makeText(getApplicationContext(),"ok,great",Toast.LENGTH_LONG).show();
+                        }
+                    });
+            dialog.setNegativeButton("cancel",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(getApplicationContext(),"close",Toast.LENGTH_LONG).show();
+                }
+            });
+            AlertDialog alertDialog=dialog.create();
+            alertDialog.show();
         }else{
             System.out.println("tidak diterima");
         }
--- app/src/main/java/com/alfansyah/multidaya/taskmultidaya/ThirdActivity.java
+++ app/src/main/java/com/alfansyah/multidaya/taskmultidaya/ThirdActivity.java
@@ -2,7 +2,13 @@ package com.alfansyah.multidaya.taskmultidaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        final EditText email = (EditText)findViewById(R.id.etEmail);
+        final EditText password = (EditText)findViewById(R.id.etPassword);
+        final Button button = (Button)findViewById(R.id.btLogin);
+        final int counter = 3;
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email.getText().toString().equals("admin@gmail.com") &&
+                        password.getText().toString().equals("admin123")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(ThirdActivity.this,HomeActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                    if (counter == 0) {
+                        button.setEnabled(false);
+                    }
+                }
+            }
+        });
+
+
     }
 }
